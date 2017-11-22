9dcb10e861dfa49e74192224247595f850ad4574
Enjl <enjlectric@gmail.com>
2017-11-22 18:54:10 +0100

    finish move

---
+++ proj/Assets/Resources/Scripts/Editor/ListDrawer.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReorderableListAttribute))]
+public class ListDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //ReorderableListAttribute listSettings = (ReorderableListAttribute)attribute;
+        //SerializedProperty targetArray = GetBaseProperty<Array>(property);
+        /*
+        string path = property.propertyPath;
+        int arrayInd = path.LastIndexOf(".Array");
+        bool bIsArray = arrayInd >= 0;
+
+        if (bIsArray)
+        {
+            SerializedObject so = property.serializedObject;
+            string arrayPath = path.Substring(0, arrayInd);
+            SerializedProperty arrayProp = so.FindProperty(arrayPath);
+
+            int indStart = path.IndexOf("[") + 1;
+            int indEnd = path.IndexOf("]");
+            string indString = path.Substring(indStart, indEnd - indStart);
+            int myIndex = int.Parse(indString);
+
+            if (myIndex == 0)
+            {
+                UnityEditorInternal.ReorderableList list = new UnityEditorInternal.ReorderableList(so, arrayProp, true, true, true, true);
+                list.DoList(position);
+            }
+        }
+        */
+        //string propName = listSettings.listName;
+        //if (string.IsNullOrEmpty(listName))
+        //    propName = property.name;
+
+        //*
+
+        string path = property.propertyPath;
+        int arrayInd = path.LastIndexOf(".Array");
+
+        string arrayPath = path.Substring(0, arrayInd);
+        SerializedProperty arrayProp = property.serializedObject.FindProperty(arrayPath);
+
+        int indStart = path.IndexOf("[") + 1;
+        int indEnd = path.IndexOf("]");
+        string indString = path.Substring(indStart, indEnd - indStart);
+        int myIndex = int.Parse(indString);
+
+        bool moveUp = false;
+        bool moveDown = false;
+
+        EditorGUI.BeginProperty(position, label, property);
+
+        EditorGUI.PropertyField(new Rect(position.x, position.y, position.width-50, position.height), property);
+        if (myIndex > 0)
+            moveUp = GUI.Button(new Rect(position.x + position.width - 40, position.y, 20, position.height), "U");
+        if (myIndex < arrayProp.arraySize-1)
+            moveDown = GUI.Button(new Rect(position.x + position.width - 20, position.y, 20, position.height), "D");
+
+        if (moveUp)
+            arrayProp.MoveArrayElement(myIndex, myIndex - 1);
+        if (moveDown)
+            arrayProp.MoveArrayElement(myIndex, myIndex + 1);
+        //EditorGUI
+        EditorGUI.EndProperty();
+        //*/
+    }
+
+    static T GetBaseProperty<T>(SerializedProperty prop)
+    {
+        // Separate the steps it takes to get to this property
+        string[] separatedPaths = prop.propertyPath.Split('.');
+
+        // Go down to the root of this serialized property
+        System.Object reflectionTarget = prop.serializedObject.targetObject as object;
+        // Walk down the path to get the target object
+        foreach (var path in separatedPaths)
+        {
+            FieldInfo fieldInfo = reflectionTarget.GetType().GetField(path);
+            reflectionTarget = fieldInfo.GetValue(reflectionTarget);
+        }
+        return (T)reflectionTarget;
+    }
+}
---
+++ proj/Assets/Resources/Scripts/Utility/PlacementTool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementTool : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+    public Transform[] toggledObjs;
+
+    [HideInInspector] public string typeName = "GenericPlacer";
+
+
+    public virtual void Reset()
+    {
+        typeName = "GenericGroup";
+    }
+
+    public virtual void Update()
+    {
+    }
+
+
+    public virtual GameObject RandomPrefab()
+    {
+        if (prefabs.Length > 0)
+            return prefabs[Random.Range(0, prefabs.Length - 1)];
+        else
+            return null;
+    }
+
+    public virtual string PrefabsName()
+    {
+        return prefabs.Length > 1 ? "Mixed Objects" : prefabs[0].name;
+    }
+
+    public virtual int PlacedCount()
+    {
+        return transform.GetChildCount();
+    }
+
+    public virtual void ChangeName()
+    {
+        gameObject.name = PrefabsName() + (PlacedCount() > 1 ? " x" + PlacedCount().ToString() : "") + " (" + typeName + ")";
+    }
+}
