893f1bdb584b4fae5ce04a10ef29d2a7a9f9c57b
Enjl <enjlectric@gmail.com>
2017-12-08 18:39:40 +0100

    updated pathedmovement

--- proj/Assets/Scripts/PathedMovement.cs
+++ proj/Assets/Scripts/PathedMovement.cs
@@ -10,20 +10,31 @@ public class MovementCommand
 {
     public enum CommandType
     {
-        Move,
-        Rotate,
-        Wait,
+        Motion,
         Despawn
     }
     public CommandType type;
-    public float duration;
-    public Vector3 direction;
+    [Tooltip("Also determines after how long the object should respawn if 'Despawn' is selected. 0 is never.")]
+    public float duration = 1;
+    public bool freezePosition = false;
+    public bool freezeRotation = false;
 
-    public MovementCommand(CommandType type, float duration, Vector3 direction)
+    [Tooltip("Whether or not the speed vectors should be interpreted as target position/rotation.")]
+    public bool interpretSpeedAsGoal;
+    [Tooltip("Moves the GameObject.")]
+    public Vector3 speed;
+    [Tooltip("Rotates the GameObject.")]
+    public Vector3 angularSpeed;
+
+    public MovementCommand(CommandType type, bool interpretSpeedAsGoal, float duration, Vector3 speed, Vector3 angularSpeed, bool freezePosition, bool freezeRotation)
     {
         this.type = type;
         this.duration = duration;
-        this.direction = direction;
+        this.speed = speed;
+        this.angularSpeed = angularSpeed;
+        this.interpretSpeedAsGoal = interpretSpeedAsGoal;
+        this.freezePosition = freezePosition;
+        this.freezeRotation = freezeRotation;
     }
 }
 
@@ -70,7 +81,7 @@ public class PathedMovement : MonoBehaviour
             foreach (MovementCommand step in steps)
             {
                 newSteps[pos] = step;
-                newSteps[newSteps.Length - 1 - pos] = new MovementCommand(step.type, step.duration, -step.direction);
+                newSteps[newSteps.Length - 1 - pos] = new MovementCommand(step.type, step.interpretSpeedAsGoal, step.duration, -step.speed, -step.angularSpeed, step.freezePosition, step.freezeRotation);
 
                 pos += 1;
             }
@@ -127,22 +138,27 @@ public class PathedMovement : MonoBehaviour
 
     private void StartNext()
     {
+        if (inRoutine && active)
+        {
+            int lastPos = currentPos - 1;
+            if (currentPos == 0)
+            {
+                lastPos = steps.Length;
+            }
+            MovementCommand m = steps[lastPos];
+            OnEventEnd(lastPos, m.type, m.duration, m.interpretSpeedAsGoal, m.speed, m.angularSpeed, m.freezePosition, m.freezeRotation);
+        }
         inRoutine = false;
 
         if (active)
         {
-            inRoutine = true;
             MovementCommand m = steps[currentPos];
+            inRoutine = true;
+            OnEventStart(currentPos,m.type, m.duration, m.interpretSpeedAsGoal, m.speed, m.angularSpeed, m.freezePosition, m.freezeRotation);
             switch (m.type)
             {
-                case MovementCommand.CommandType.Move:
-                    StartCoroutine(Move(m.duration, m.direction));
-                    break;
-                case MovementCommand.CommandType.Rotate:
-                    StartCoroutine(Rotate(m.duration, m.direction));
-                    break;
-                case MovementCommand.CommandType.Wait:
-                    StartCoroutine(Wait(m.duration));
+                case MovementCommand.CommandType.Motion:
+                    StartCoroutine(Motion(m.duration, m.speed, m.angularSpeed, m.interpretSpeedAsGoal, m.freezePosition, m.freezeRotation));
                     break;
                 case MovementCommand.CommandType.Despawn:
                     StartCoroutine(Despawn(m.duration));
@@ -157,60 +173,67 @@ public class PathedMovement : MonoBehaviour
         }
     }
 
-    private IEnumerator Move(float time, Vector3 direction)
+    private IEnumerator Motion(float time, Vector3 speed, Vector3 angularSpeed, bool isGoal, bool freezeP, bool freezeR)
     {
+
         float startTime = 0;
-        Vector3 startPosition = transform.position;
+        Vector3 modifiedSpeed = speed;
+        Vector3 modifiedAngular = angularSpeed;
+
+        if (isGoal)
+        {
+            if (movementSpace == Space.Self)
+            {
+                modifiedSpeed = (speed - transform.localPosition) / time;
+                modifiedAngular = (angularSpeed - transform.localEulerAngles) / time;
+            }
+            else
+            {
+                modifiedSpeed = (speed - transform.position) / time;
+                modifiedAngular = (angularSpeed - transform.eulerAngles) / time;
+            }
+        }
+
         while (startTime < time)
         {
             float t = Time.deltaTime;
-            transform.Translate(direction * t, movementSpace);
+            if (!freezeP)
+            {
+                transform.Translate(modifiedSpeed * t, movementSpace);
+            }
+            if (!freezeR)
+            {
+                transform.Rotate(modifiedAngular * t, movementSpace);
+            }
             startTime += t;
-            if (useBattery)
-            {
-                while (!attachedBattery.GetActive())
-                {
-                    yield return null;
-                }
-            }
-            yield return null;
+            yield return StartCoroutine(Wait());
         }
-        StartNext();
-    }
-    private IEnumerator Wait(float time)
-    {
-        float startTime = 0;
-        while (startTime < time)
+
+        if (isGoal)
         {
-            startTime += Time.deltaTime;
-            if (useBattery)
+            if (!freezeP)
             {
-                while (!attachedBattery.GetActive())
+                if (movementSpace == Space.Self)
                 {
-                    yield return null;
+                    transform.localPosition = speed;
+                } else
+                {
+                    transform.position = speed;
                 }
             }
-            yield return null;
-        }
-        StartNext();
-    }
-    private IEnumerator Rotate(float time, Vector3 direction)
-    {
-        float startTime = 0;
-        while (startTime < time)
-        {
-            float t = Time.deltaTime;
-            transform.Rotate(direction * t, movementSpace);
-            startTime += t;
-            if (useBattery)
+            if (!freezeR)
             {
-                while (!attachedBattery.GetActive())
+                if (movementSpace == Space.Self)
                 {
-                    yield return null;
+                    transform.localEulerAngles = speed;
+                }
+                else
+                {
+                    transform.eulerAngles = speed;
                 }
             }
-            yield return null;
         }
+
         StartNext();
     }
 
@@ -230,7 +253,6 @@ public class PathedMovement : MonoBehaviour
         if (time > 0)
         {
             yield return new WaitForSeconds(time);
-            inRoutine = false;
             Reset();
 
             foreach (Transform child in children)
@@ -239,4 +261,65 @@ public class PathedMovement : MonoBehaviour
             }
         }
     }
+
+    private IEnumerator Wait()
+    {
+        if (useBattery)
+        {
+            while (!attachedBattery.GetActive())
+            {
+                yield return null;
+            }
+        }
+        yield return null;
+    }
+
+    public virtual void OnEventStart(int index, MovementCommand.CommandType type, float duration, bool isGoal, Vector3 speed, Vector3 angularSpeed, bool freezeP, bool freezeR)
+    {
+
+    }
+
+    public virtual void OnEventEnd(int index, MovementCommand.CommandType type, float duration, bool isGoal, Vector3 speed, Vector3 angularSpeed, bool freezeP, bool freezeR)
+    {
+
+    }
+
+
+
+    /*private IEnumerator Rotate(float time, Vector3 speed, bool isGoal)
+    {
+        float startTime = 0;
+        Vector3 modifiedSpeed = speed;
+
+        if (isGoal)
+        {
+            modifiedSpeed /= time;
+        }
+
+        while (startTime < time)
+        {
+            float t = Time.deltaTime;
+            transform.Rotate(modifiedSpeed * t, movementSpace);
+            startTime += t;
+            yield return StartCoroutine(Wait());
+        }
+
+        if (isGoal)
+        {
+            transform.eulerAngles = speed;
+        }
+
+        StartNext();
+    }
+
+    private IEnumerator Wait(float time)
+    {
+        float startTime = 0;
+        while (startTime < time)
+        {
+            startTime += Time.deltaTime;
+            yield return StartCoroutine(Wait());
+        }
+        StartNext();
+    }*/
 }
