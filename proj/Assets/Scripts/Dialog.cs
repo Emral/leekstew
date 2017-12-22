8cb957f6f5962d8891549afcac1aa537fdc2397b
Hoeloe <hoeloe@hotmail.co.uk>
2017-12-22 19:52:09 +0000

    Shifted the "progress dialogue" button to stop it obscuring text (it still does sometimes at 4K resolution, but eh)

--- proj/Assets/Scripts/Dialog.cs
+++ proj/Assets/Scripts/Dialog.cs
@@ -93,8 +93,8 @@ public class Dialog : MonoBehaviour {
 
             if (animPercent == 1 && !passive)
             {
-                GUI.DrawTexture(new Rect(boxRect.xMax - Screen.width * 0.015f, 
-                                         boxRect.yMax - Screen.width * 0.015f, 
+                GUI.DrawTexture(new Rect(boxRect.xMax - Screen.width * 0.0075f, 
+                                         boxRect.yMax - Screen.width * 0.0075f, 
                                          Screen.width * 0.03f, 
                                          Screen.width * 0.03f), 
                                 UIManager.TalkTextureAnimated);
